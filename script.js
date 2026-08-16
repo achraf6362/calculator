ad4dc8de25d50fa9cd5bfef693a40282c4ee11df
A <123079127+achraf6362@users.noreply.github.com>
2026-08-16 08:37:18 +0100

    Code restructuring

--- script.js
+++ script.js
@@ -1,5 +1,166 @@
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
 const display = document.querySelector('#display');
 const equal = document.querySelector('#equal');
+const clear = document.querySelector('#clear');
+const del = document.querySelector('#del');
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => b === 0 ? 'Error' : a / b;
+
+let aNum = '';
+let bNum = '';
+let active = 'a';
+let o = '';
+let oActive = 'yes';
+let newCalc = 'yes';
+
+function operate(a, b, operator) {
+  if (operator === '+') return add(a, b, operator);
+  else if (operator === '-') return subtract(a, b, operator);
+  else if (operator === '*') return multiply(a, b, operator);
+  else if (operator === '/') return divide(a, b, operator);
+}
+
+numbers.forEach(number => {
+  number.addEventListener('click', (e) => {
+    if (newCalc === 'yes' && aNum !== '') {
+      display.textContent = '';
+      aNum = '';
+      bNum = '';
+      active = 'a';
+      newCalc = 'no';
+    }
+
+    if (active === 'a') {
+      newCalc = 'no';
+      aNum += e.target.value;
+      display.textContent += e.target.value;
+      // if (aNum.includes('.')) return;
+    } else if (active === 'b') {
+        bNum += e.target.value;
+        display.textContent += e.target.value;
+        oActive = 'no';
+    }
+
+    
+  });
+});
+
+operators.forEach(operator => {
+  operator.addEventListener('click', (e) => {
+    if (aNum === '' || aNum === 'Error') return;
+
+    newCalc = 'no';
+
+    if (active === 'a' && oActive === 'yes') {
+      active = 'b';
+      o = e.target.value;
+      display.textContent += e.target.value;
+    } else if (active === 'b' && o !== '' && oActive === 'yes') {
+      display.textContent = display.textContent.slice(0, -1) + e.target.value;
+      o = e.target.value;
+    } else if (active === 'b' && bNum !== '') {
+      let result = operate(+aNum, +bNum, o);
+      o = e.target.value;
+      display.textContent = result + o;
+      aNum = result;
+      bNum = '';
+      oActive = 'yes';
+    }
+  });
+});
+
+equal.addEventListener('click', () => {
+  if (bNum !== '') {
+    let result = operate(+aNum, +bNum, o);
+    display.textContent = result;
+    aNum = result;
+    bNum = '';
+    active = 'a';
+    o = '';
+    oActive = 'yes';
+    newCalc = 'yes';
+  }
+});
+
+clear.addEventListener('click', () => {
+  display.textContent = '';
+  aNum = '';
+  bNum = '';
+  active = 'a';
+  o = '';
+  oActive = 'yes';
+  newCalc = 'yes';
+});
+
+
+/* del.addEventListener('click', () => {
+  if (active === 'a') {
+    aNum = aNum.slice(0, -1);
+    display.textContent = display.textContent.slice(0, -1);
+  }
+}) */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const display = document.querySelector('#display');
+const equal = document.querySelector('#equal');
+const del = document.querySelector('#del');
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 
@@ -75,3 +236,12 @@ equal.addEventListener('click', () => {
   p = 'y';
 });
 
+del.addEventListener('click', () => {
+  if (c === 'a') {
+    display.textContent = display.textContent.slice(0, -1);
+    num1 = num1.slice(0, -1);
+  } else {
+    display.textContent = display.textContent.slice(0, -1);;
+    num2 = num2.slice(0, -1);
+  }
+}) */
