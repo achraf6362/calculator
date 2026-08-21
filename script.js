38a0cfcecbcc958f846c1e30e2e519ed017fd284
A <123079127+achraf6362@users.noreply.github.com>
2026-08-21 10:51:56 +0100

    Add two displays

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
-const display = document.querySelector('#display');
+const input = document.querySelector('#input');
+const result = document.querySelector('#result');
 const equal = document.querySelector('#equal');
 const clear = document.querySelector('#clear');
 const del = document.querySelector('#del');
@@ -29,8 +30,12 @@ function operate(a, b, operator) {
 
 numbers.forEach(number => {
   number.addEventListener('click', (e) => {
+    if (isANumActive && aNum.length >= 12) return;
+    if (!isANumActive && bNum.length >= 12) return;
+
+
     if (isNewCalc && aNum !== '') {
-      display.textContent = '';
+      result.textContent = '';
       aNum = '';
       bNum = '';
       isANumActive = true;
@@ -54,7 +59,7 @@ numbers.forEach(number => {
         aNum += e.target.value;
       }
 
-      display.textContent = aNum;
+      result.textContent = aNum;
       
     } else if (!isANumActive) {
         if (e.target.value === '.' && bNum.includes('.')) return;
@@ -63,11 +68,11 @@ numbers.forEach(number => {
 
         if (bNum === '' && e.target.value === '.') {
           bNum = '0';
-          display.textContent += bNum;
+          result.textContent += bNum;
         }
 
         bNum += e.target.value;
-        display.textContent += e.target.value;
+        result.textContent = bNum;
         isOActive = false;
     }
   });
@@ -81,32 +86,31 @@ operators.forEach(operator => {
     
     if (aNum === 'Error') return;
 
-
     if (isANumActive && isOActive && aNum === '') {
       aNum = '0';
       isANumActive = false;
       o = e.target.value;
-      display.textContent += e.target.textContent;
+      input.textContent += aNum + ' ' + ' ' + e.target.textContent;
     }
     
     else if (isANumActive && isOActive) {
       isANumActive = false;
       o = e.target.value;
-        display.textContent += e.target.textContent;
+      input.textContent += aNum + ' ' + ' ' + e.target.textContent;
     }
     
     else if (!isANumActive && o !== '' && isOActive) {
-      display.textContent = display.textContent.slice(0, -1) + e.target.textContent;
+      input.textContent = aNum + e.target.textContent;
       o = e.target.value;
     }
     
     else if (!isANumActive && bNum !== '') {
-      let result = Math.round(operate(+aNum, +bNum, o) * 10000000000) / 10000000000;
-      if (!isFinite(result)) result = 'Error';
+      let r = Math.round(operate(+aNum, +bNum, o) * 10000000000) / 10000000000;
+      if (!isFinite(r)) r = 'Error';
       o = e.target.value;
-      display.textContent = result + e.target.textContent;
-      aNum = String(result);
+      aNum = String(r);
       bNum = '';
+      input.textContent = aNum + e.target.textContent;
       isOActive = true;
     }
   });
@@ -115,10 +119,11 @@ operators.forEach(operator => {
 equal.addEventListener('click', () => {
 
   if (bNum !== '') {
-    let result = Math.round(operate(+aNum, +bNum, o) * 10000000000) / 10000000000;
-    if (!isFinite(result)) result = 'Error';
-    display.textContent = result;
-    aNum = String(result);
+    let r = Math.round(operate(+aNum, +bNum, o) * 10000000000) / 10000000000;
+    if (!isFinite(r)) r = 'Error';
+    result.textContent = r;
+    input.textContent += ' ' + bNum + ' ' + '=';
+    aNum = String(r);
     lastBNum = bNum;
     LastO = o;
     bNum = '';
@@ -129,16 +134,16 @@ equal.addEventListener('click', () => {
   }
 
   else if (lastBNum !== '' && LastO !== '' && aNum !== '' && aNum !== 'Error') {
-    let result = Math.round(operate(+aNum, +lastBNum, LastO) * 10000000000) / 10000000000;
-    if (!isFinite(result)) result = 'Error';
-    display.textContent = result;
-    aNum = String(result);
+    let r = Math.round(operate(+aNum, +lastBNum, LastO) * 10000000000) / 10000000000;
+    if (!isFinite(r)) r = 'Error';
+    result.textContent = r;
+    aNum = String(r);
     isNewCalc = true;
   }
 });
 
 clear.addEventListener('click', () => {
-  display.textContent = '0';
+  result.textContent = '0';
   aNum = '';
   bNum = '';
   isANumActive = true;
@@ -154,20 +159,20 @@ del.addEventListener('click', () => {
 
   if (bNum !== '') {
     bNum = bNum.slice(0, -1);
-    display.textContent = display.textContent.slice(0, -1);
+    result.textContent = result.textContent.slice(0, -1);
   }
   
   else if (o !== '') {
     o = '';
     isANumActive = true;
     isOActive = true;
-    display.textContent = display.textContent.slice(0, -1);
+    result.textContent = result.textContent.slice(0, -1);
   }
   
   else if (isANumActive) {
     aNum = aNum.slice(0, -1);
-    display.textContent = display.textContent.slice(0, -1);
-    if (display.textContent === '') display.textContent = '0';
+    result.textContent = result.textContent.slice(0, -1);
+    if (result.textContent === '') result.textContent = '0';
   }
 });
 
@@ -189,12 +194,12 @@ pn.addEventListener('click', () => {
 
   if (isANumActive) {
     aNum = aNum.startsWith('-') ? aNum.slice(1) : '-' + aNum;
-    display.textContent = aNum;
+    result.textContent = aNum;
   }
   
   else if (!isANumActive && bNum !== '') {
     bNum = bNum.startsWith('-') ? bNum.slice(1) : '-' + bNum;
-    display.textContent = aNum + o + bNum;
+    result.textContent = aNum + o + bNum;
   }
 });
 
